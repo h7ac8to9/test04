cdd99d59780d19c13b9ca23a64ebd002b8d82bae
yat_pc <h7ac8to9@gmail.com>
2017-08-30 01:22:31 +0900

    ついか

---
+++ android/src/com/jgf/test04/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.jgf.test04;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.jgf.test04.MyGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGame(), config);
+	}
+}
---
+++ desktop/src/com/jgf/test04/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jgf.test04.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jgf.test04.MyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGame(), config);
+	}
+}
